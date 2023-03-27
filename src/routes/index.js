e532ea765d48fbbdcc3439d7388f395c8972550c
Lili Cardoso <lili200cardoso@outlook.com.br>
2023-03-27 18:34:14 -0300

    Exercicios faculdade

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import { createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Welcome from '../pages/Welcome'
+import Signin from '../pages/Signin'
+
+const stack = createNativeStackNavigator();
+
+export default function Routes(){
+    return (
+        <stack.Navigator>
+            <stack.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{ headerShown: false }}
+            />
+
+            <stack.Screen
+                name="Signin"
+                component={Signin}
+                options={{ headerShown: false }}
+            />
+
+        </stack.Navigator>
+    )
+}
